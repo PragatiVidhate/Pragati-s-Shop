86b8dc301a53237b39149681950ade4de9ae44da
PragatiVidhate <117562465+PragatiVidhate@users.noreply.github.com>
2022-11-06 23:37:37 +0530

    Add files via upload

---
+++ AddCategory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pragati
+{
+    public partial class AddCategory : Form
+    {
+        public AddCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtcategoryID.Text != "" && txtcategoryName.Text != "" && textTotalProduct.Text != "")
+            {
+                String cid = txtcategoryID.Text;
+                String cname = txtcategoryName.Text;
+                String tp = textTotalProduct.Text;
+
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "Data Source=DESKTOP-M9968TV;Initial Catalog=Pragati;Integrated Security=True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                con.Open();
+                cmd.CommandText = "insert into Category (Category_id,Category_Name,Total_Products) values (" + cid + ",'" + cname + "'," + tp + ");";
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Data Saved", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtcategoryID.Clear();
+                txtcategoryName.Clear();
+                textTotalProduct.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Empty Field NOT Allowed", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("This will DELETE your unsaved DATA.", "Are You Sure?)", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+
+            {
+                this.Close();
+            }
+        }
+    }
+}
