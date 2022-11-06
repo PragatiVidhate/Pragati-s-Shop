26013f40b396a1381e445106737d2c072fe0604d
PragatiVidhate <117562465+PragatiVidhate@users.noreply.github.com>
2022-11-06 23:38:12 +0530

    Add files via upload

---
+++ AddCategory.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Pragati
+{
+    partial class AddCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtcategoryID = new System.Windows.Forms.TextBox();
+            this.txtcategoryName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancle = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textTotalProduct = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add New Category";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(134, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Category ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(134, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Category Name";
+            // 
+            // txtcategoryID
+            // 
+            this.txtcategoryID.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcategoryID.Location = new System.Drawing.Point(445, 174);
+            this.txtcategoryID.Name = "txtcategoryID";
+            this.txtcategoryID.Size = new System.Drawing.Size(262, 38);
+            this.txtcategoryID.TabIndex = 3;
+            // 
+            // txtcategoryName
+            // 
+            this.txtcategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcategoryName.Location = new System.Drawing.Point(445, 252);
+            this.txtcategoryName.Name = "txtcategoryName";
+            this.txtcategoryName.Size = new System.Drawing.Size(262, 38);
+            this.txtcategoryName.TabIndex = 4;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(260, 422);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(80, 31);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancle
+            // 
+            this.btnCancle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancle.Location = new System.Drawing.Point(528, 422);
+            this.btnCancle.Name = "btnCancle";
+            this.btnCancle.Size = new System.Drawing.Size(80, 31);
+            this.btnCancle.TabIndex = 6;
+            this.btnCancle.Text = "Cancle";
+            this.btnCancle.UseVisualStyleBackColor = true;
+            this.btnCancle.Click += new System.EventHandler(this.btnCancle_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(134, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Total Product";
+            // 
+            // textTotalProduct
+            // 
+            this.textTotalProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTotalProduct.Location = new System.Drawing.Point(445, 321);
+            this.textTotalProduct.Name = "textTotalProduct";
+            this.textTotalProduct.Size = new System.Drawing.Size(262, 38);
+            this.textTotalProduct.TabIndex = 8;
+            // 
+            // AddCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 573);
+            this.Controls.Add(this.textTotalProduct);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCancle);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtcategoryName);
+            this.Controls.Add(this.txtcategoryID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AddCategory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AddCategory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtcategoryID;
+        private System.Windows.Forms.TextBox txtcategoryName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textTotalProduct;
+    }
+}
