4d4e7821a107cc10196c7566dc383a6eab2150db
PragatiVidhate <117562465+PragatiVidhate@users.noreply.github.com>
2022-11-06 23:56:20 +0530

    Add files via upload

---
+++ ViewC.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Pragati
+{
+    partial class ViewC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.butDelete = new System.Windows.Forms.Button();
+            this.bCancle = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textTotalProduct = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textcategoryName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtcategoryID = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(281, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "View Category";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 199);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(852, 259);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagridviewcellclick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Category ID";
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsearch.Location = new System.Drawing.Point(354, 112);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(262, 38);
+            this.txtsearch.TabIndex = 4;
+            this.txtsearch.TextChanged += new System.EventHandler(this.Textchange);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(91, 191);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(80, 31);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // butDelete
+            // 
+            this.butDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butDelete.Location = new System.Drawing.Point(270, 191);
+            this.butDelete.Name = "butDelete";
+            this.butDelete.Size = new System.Drawing.Size(80, 31);
+            this.butDelete.TabIndex = 7;
+            this.butDelete.Text = "Delete";
+            this.butDelete.UseVisualStyleBackColor = true;
+            this.butDelete.Click += new System.EventHandler(this.butDelete_Click);
+            // 
+            // bCancle
+            // 
+            this.bCancle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCancle.Location = new System.Drawing.Point(437, 191);
+            this.bCancle.Name = "bCancle";
+            this.bCancle.Size = new System.Drawing.Size(80, 31);
+            this.bCancle.TabIndex = 8;
+            this.bCancle.Text = "Cancle";
+            this.bCancle.UseVisualStyleBackColor = true;
+            this.bCancle.Click += new System.EventHandler(this.bCancle_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Category ID";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textTotalProduct);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textcategoryName);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.bCancle);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.butDelete);
+            this.panel1.Controls.Add(this.txtcategoryID);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(43, 496);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(790, 254);
+            this.panel1.TabIndex = 10;
+            // 
+            // textTotalProduct
+            // 
+            this.textTotalProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTotalProduct.Location = new System.Drawing.Point(270, 123);
+            this.textTotalProduct.Name = "textTotalProduct";
+            this.textTotalProduct.Size = new System.Drawing.Size(262, 38);
+            this.textTotalProduct.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(196, 32);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Total Product";
+            // 
+            // textcategoryName
+            // 
+            this.textcategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textcategoryName.Location = new System.Drawing.Point(270, 71);
+            this.textcategoryName.Name = "textcategoryName";
+            this.textcategoryName.Size = new System.Drawing.Size(262, 38);
+            this.textcategoryName.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 32);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Category Name";
+            // 
+            // txtcategoryID
+            // 
+            this.txtcategoryID.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcategoryID.Location = new System.Drawing.Point(270, 15);
+            this.txtcategoryID.Name = "txtcategoryID";
+            this.txtcategoryID.Size = new System.Drawing.Size(262, 38);
+            this.txtcategoryID.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(647, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 31);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ViewC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(858, 778);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ViewC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ViewC";
+            this.Load += new System.EventHandler(this.ViewC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button butDelete;
+        private System.Windows.Forms.Button bCancle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtcategoryID;
+        private System.Windows.Forms.TextBox textcategoryName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textTotalProduct;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
