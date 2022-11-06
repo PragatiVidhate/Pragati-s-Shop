0af37b17c4872ebcf44d03bc9cf9861d20c25ad2
PragatiVidhate <117562465+PragatiVidhate@users.noreply.github.com>
2022-11-06 23:50:07 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Pragati
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.caregoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewCategoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewCategoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.productToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewProductToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewProductToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(195, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRAGATI\'S STORE";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.caregoryToolStripMenuItem,
+            this.productToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(696, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // caregoryToolStripMenuItem
+            // 
+            this.caregoryToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewCategoryToolStripMenuItem,
+            this.viewCategoriesToolStripMenuItem});
+            this.caregoryToolStripMenuItem.Name = "caregoryToolStripMenuItem";
+            this.caregoryToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.caregoryToolStripMenuItem.Text = "Category";
+            // 
+            // addNewCategoryToolStripMenuItem
+            // 
+            this.addNewCategoryToolStripMenuItem.Name = "addNewCategoryToolStripMenuItem";
+            this.addNewCategoryToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.addNewCategoryToolStripMenuItem.Text = "Add New Category";
+            this.addNewCategoryToolStripMenuItem.Click += new System.EventHandler(this.addNewCategoryToolStripMenuItem_Click);
+            // 
+            // viewCategoriesToolStripMenuItem
+            // 
+            this.viewCategoriesToolStripMenuItem.Name = "viewCategoriesToolStripMenuItem";
+            this.viewCategoriesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.viewCategoriesToolStripMenuItem.Text = "View Categories";
+            this.viewCategoriesToolStripMenuItem.Click += new System.EventHandler(this.viewCategoriesToolStripMenuItem_Click);
+            // 
+            // productToolStripMenuItem
+            // 
+            this.productToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewProductToolStripMenuItem,
+            this.viewProductToolStripMenuItem});
+            this.productToolStripMenuItem.Name = "productToolStripMenuItem";
+            this.productToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.productToolStripMenuItem.Text = "Product";
+            // 
+            // addNewProductToolStripMenuItem
+            // 
+            this.addNewProductToolStripMenuItem.Name = "addNewProductToolStripMenuItem";
+            this.addNewProductToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.addNewProductToolStripMenuItem.Text = "Add New Product";
+            this.addNewProductToolStripMenuItem.Click += new System.EventHandler(this.addNewProductToolStripMenuItem_Click);
+            // 
+            // viewProductToolStripMenuItem
+            // 
+            this.viewProductToolStripMenuItem.Name = "viewProductToolStripMenuItem";
+            this.viewProductToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.viewProductToolStripMenuItem.Text = "View Product";
+            this.viewProductToolStripMenuItem.Click += new System.EventHandler(this.viewProductToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 539);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem caregoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewCategoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewCategoriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem productToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewProductToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewProductToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pragati
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addNewCategoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            AddCategory AC = new AddCategory();
+            AC.Show();
+        }
+
+        private void viewCategoriesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ViewC viewC = new ViewC();
+            viewC.Show();
+        }
+
+        private void addNewProductToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddProducts addProducts = new AddProducts();
+            addProducts.Show();
+        }
+
+        private void viewProductToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Products viewProducts = new Products();
+            viewProducts.Show();
+        }
+    }
+}
